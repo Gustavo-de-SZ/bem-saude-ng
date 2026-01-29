19a80823ff60dd9a0ba34da6902ec55e27ee5f4f
Gustavo de Souza <gudesouza.sz@gmail.com>
2026-01-28 21:51:39 -0300

    recepcionistas

---
+++ src/app/core/components/registro-status-tag/registro-status-tag.ts
@@ -0,0 +1,10 @@
+import { Component, input } from '@angular/core';
+import {TagModule} from "primeng/tag"
+@Component({
+  selector: 'app-registro-status-tag',
+  imports: [TagModule],
+  templateUrl: './registro-status-tag.html',
+})
+export class RegistroStatusTag {
+  status = input<boolean>();
+}
